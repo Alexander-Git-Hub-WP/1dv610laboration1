7ef3651baf963420b2265bf7a445f1e20f028a04
Alexander Skogberg <as229kz@student.lnu.se>
2026-09-07 23:24:32 +0200

    Add greeting application with JSDoc and ASCII frame

---
+++ src/app.js
@@ -0,0 +1,24 @@
+/**
+ * @file A simple greeting application with a ASCII frame.
+ * @author Alexander Skogberg <as229kz@student.lnu.se>
+ * @version 1.0.0
+ */
+
+const readline = require('readline');
+
+const terminal = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+terminal.question('What is your name? ', (namn) => {
+
+    const welcomeMessage = `*  WELCOME BACK FROM THE SUMMER BREAK, ${namn.toUpperCase()}!  *`
+    const rowWithStars = '*'.repeat(welcomeMessage.length)
+
+    console.log('\n' + rowWithStars)
+    console.log(welcomeMessage)
+    console.log(rowWithStars + '\n')
+
+    terminal.close()
+})
